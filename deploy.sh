f29449f7ea0d2d06bddd0574bdb7e1501048cf14
ǝuo ɔᴉɹʇuǝɔɔǝ ǝɥ┴ <159409245+xntrek@users.noreply.github.com>
2025-09-06 21:26:40 +1000

    🫰

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-cd ~/Library/CloudStorage/Tresorit-TaissQuartapa/Notes/gitRepos/xntrek.github.io
+cd ~/gitRepos/xntrek.github.io
 
 chruby 3.4.1
+bundle exec jekyll clean 
 bundle init
 bundle update
 bundle
-bundle exec jekyll clean 
 bundle exec jekyll build timezone: Australia/Melbourne
